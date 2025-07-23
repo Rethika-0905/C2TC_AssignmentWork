60ec03d6dc4b30169f22311489eda300c742c3ad
rethi <rethi@MyPC26>
2025-07-23 20:30:20 +0530

    Assignment on constructor and methods.

---
+++ C2TC_Assignment/src/Assignment2/CommissionDemo.java
@@ -0,0 +1,12 @@
+package Assignment2;
+
+public class CommissionDemo {
+
+	public static void main(String[] args) {
+		Commission c  = new Commission();
+		c.details();
+		c.calculate();
+		
+		System.out.println("Successfully completed!!");
+	}
+}
